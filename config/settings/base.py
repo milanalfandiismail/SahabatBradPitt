c0e9eae2a6b1aab31591300dd1df2cc372f4f92f
milanalfandiismail <milan.gcenter1@gmail.com>
2026-05-28 20:54:08 +0800

    Menambahkan gunicorn dan whitenoise untuk environment production

--- config/settings/base.py
+++ config/settings/base.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',  # CORS
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
